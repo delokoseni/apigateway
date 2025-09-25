61e6a33463b72726b099f1a8559adc5253f93823
delokoseni <artur.b3lsh@gmail.com>
2025-09-25 14:45:19 +0700

    Added Kitchen microservice and orders_log file.

---
+++ kitchen/src/main/java/org/example/Kitchen.java
@@ -0,0 +1,19 @@
+package org.example;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.DeliverCallback;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.concurrent.TimeoutException;
+
+public class Kitchen {
+
+    private static final String KITCHEN_QUEUE_NAME = "kitchen_queue";
+
+    public static void main(String[] args) {
+
+    }
+}
--- orders/src/main/java/org/example/Orders.java
+++ orders/src/main/java/org/example/Orders.java
@@ -5,6 +5,7 @@ import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
 import com.rabbitmq.client.DeliverCallback;
 
+import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.util.concurrent.TimeoutException;
@@ -12,6 +13,8 @@ import java.util.concurrent.TimeoutException;
 public class Orders {
 
     private static final String ORDER_QUEUE_NAME = "order_queue";
+    private static int ORDER_NUMBER = 1;
+    private static final String FILE_NAME = "orders_log.txt";
 
     public static void main(String[] args) throws IOException, TimeoutException {
         ConnectionFactory factory = new ConnectionFactory();
@@ -25,7 +28,17 @@ public class Orders {
         DeliverCallback deliverCallback = (consumerTag, delivery) -> {
             String message = new String(delivery.getBody(), StandardCharsets.UTF_8);
             System.out.println(" [x] Received '" + message + "'");
+            saveToDB(message);
         };
         channel.basicConsume(ORDER_QUEUE_NAME, true, deliverCallback, consumerTag -> { });
+    }
+
+    private static void saveToDB(String order) {
+        try (FileWriter writer = new FileWriter(FILE_NAME, true)) {
+            String line = String.format("Заказ №%d | %s | Статус: Выполняется%n", ORDER_NUMBER++, order);
+            writer.write(line);
+        } catch (IOException e) {
+            System.err.println("Error: " + e.getMessage());
+        }
     }
 }
